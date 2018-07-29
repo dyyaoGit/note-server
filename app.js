63cdf47b5e9ef71329b0a954931cac95fc83f2fd
yao <6466617513@qq.com>
2018-07-29 13:50:46 +0800

    修改最大传输

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: '4mb'}));
+app.use(bodyParser.urlencoded({ limit: '4mb',extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'uploads')));
