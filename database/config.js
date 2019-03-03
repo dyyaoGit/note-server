488a9859347bfbcfc64b6021ef3d88edc18126e3
dyyao <646617513@qq.com>
2019-03-04 01:00:45 +0800

    修改配置文件

--- database/config.js
+++ database/config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 console.log(process.env.NODE_ENV)
-const connectAddress = process.env.NODE_ENV == 'development' ? 'mongodb://localhost/note' : 'mongodb://dyyao:yjr1923521@116.62.145.106:27017/note';
+const connectAddress = process.env.NODE_ENV == 'development' ? 'mongodb://localhost:27017/note' : 'mongodb://dyyao:yjr1923521@116.62.145.106:27017/note';
 mongoose.connect(
     // 'mongodb://yao:yjr1923521@localhost:27017/note',
     connectAddress,
